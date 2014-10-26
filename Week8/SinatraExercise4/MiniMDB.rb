7c9cf6b129f59304135f9cb6eb0754cd68ca166c
Ivan Rodriguez <frivanrodriguez@gmail.com>
2014-10-26 18:53:23 +0100

    26oct14 Ex4 Week 8 solved with AJAX modification

--- Week8/SinatraExercise4/MiniMDB.rb
+++ Week8/SinatraExercise4/MiniMDB.rb
@@ -5,53 +5,15 @@ require 'json'
 
 @showHash = {}
 @showJson = @showHash.to_json
-
+selectedShows = {}
+#index View
 get '/' do 
+	@selectedShows = selectedShows
 	erb :index
 end
 
-post '/list' do
-	if params["showType"] == "tvShow"
-	end
-	
-	if request.xhr?
-		@show = Imdb::Search.new(params["showName"])
-		@showList = []
-		@show.movies.each do |item|
-			@showList.push(item.title)
-		end
-		@showHash ={'showList' => @showList}
-		@showJson = @showHash.to_json
-	else
-	    return @showJson
-	end
-end
-
-get '/hello/data.json' do
-	@show = Imdb::Search.new(params["showName"])
-	@showList = []
-	@show.movies.each do |item|
-		@showList.push(item.title)
-	end
-	@showHash ={'showList' => @showList}
-	@showJson = @showHash.to_json
-end
+#'/list2' views to allow AJAX
 x=''
-get '/list2' do 
-	if request.xhr?
-		#@x = request.body.read.to_s
-		@show = Imdb::Search.new(x)
-		@showList = []
-		@show.movies.each do |item|
-			@showList.push(item.title)
-		end
-		@showHash ={'showList' => @showList}
-		@showJson = @showHash.to_json
-		return @showJson
-	else
-		return 'error'
-	end
-end
 
 post '/list2' do 
 	if request.xhr?
@@ -61,3 +23,48 @@ post '/list2' do
 		return 'error'
 	end
 end
+
+get '/list2' do 
+	if request.xhr?
+		#@x = request.body.read.to_s
+		@show = Imdb::Search.new(x)
+		@showList = []
+		@show.movies.each do |item|
+			@showList.push([item.title, item.id])
+		end
+		@showHash ={'showList' => @showList}
+		@showJson = @showHash.to_json
+		return @showJson
+	else
+		return 'error'
+	end
+end
+
+#'/show' view to add shows
+
+
+post '/show' do 
+	movie = Imdb::Movie.new(params[:showType])
+	@title = movie.title
+	@rating = movie.rating
+	@cover = movie.poster
+	@url = movie.url
+
+	if @rating.to_f > 9
+		@class = 'darkGreen'
+	elsif @rating.to_f > 8
+		@class = 'green'
+	elsif @rating.to_f > 5.5
+		@class = 'yellow'
+	else
+		@class = 'red'
+	end
+			
+			
+
+	@selectedShows = selectedShows
+	@selectedShows[params[:showType]] = [@title, @rating, @cover, @url, @class]
+	selectedShows = @selectedShows
+	erb :show
+end
+
--- Week8/SinatraExercise4/public/js/miniMdb.js
+++ Week8/SinatraExercise4/public/js/miniMdb.js
@@ -1,4 +1,4 @@
-document.getElementsByTagName('button')[0].addEventListener('click',getPost);
+document.getElementsByTagName('button')[0].addEventListener('click',createList);
 
 function getPost(){
 
@@ -9,6 +9,27 @@ function getPost(){
 	});
 	var response = $.get( "/list2");
 	
-	document.getElementById('input1').value=response.responseText;
-	return response
+	//document.getElementById('input1').value=response.responseText;
+	return response.responseText
+}
+
+function deleteList(){
+	showList = document.getElementsByClassName('showList');
+	
+	while (showList.length > 0){
+		showList[0].remove();
+	};
+}
+
+function createList(){
+	deleteList();
+	newList = JSON.parse(getPost());
+	for (i=0; i < newList.showList.length; i++){
+		option = document.createElement('option');
+		option.className = "showList";
+		option.innerHTML = newList.showList[i][0];
+		option.setAttribute('value', newList.showList[i][1]);
+		document.getElementById('show').appendChild(option);
+	}
+
 }
